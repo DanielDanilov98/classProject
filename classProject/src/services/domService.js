9c70fb8432cd56f041335a7790a3d5bb2472e414
Daniel Danilov <danieldanilov1998@gmail.com>
2023-01-08 20:40:29 +0200

    commit

--- classProject/src/services/domService.js
+++ classProject/src/services/domService.js
@@ -44,3 +44,34 @@ export const PRICE_CREATE_PIC_ERROR = document.querySelector("#price-error-span"
 export const SUBMIT_CREATE_PIC_BTN = document.querySelector("#submit-create-pic-btn");
 export const CANCEL_BTN = document.querySelector("#cancel-create-pic-btn");
 //#endregion
+
+//#region create user
+
+export const FIRST_NAME_SIGNUP_FIELD = document.querySelector("#first-name-field");
+export const FIRST_NAME_SIGNUP_ERROR = document.querySelector("#first-name-error-span");
+export const LAST_NAME_SIGNUP_FIELD = document.querySelector("#last-name-field");
+export const LAST_NAME_SIGNUP_ERROR = document.querySelector("#last-name-error-span");
+export const EMAIL_SIGNUP_FIELD = document.querySelector("#email-field");
+export const EMAIL_SIGNUP_ERROR = document.querySelector("#email-error-span");
+export const PASSWORD_SIGNUP_FIELD = document.querySelector("#password-field");
+export const PASSWORD_SIGNUP_ERROR = document.querySelector("#password-error-span");
+export const STATE_SIGNUP_FIELD = document.querySelector("#state-field");
+export const STATE_SIGNUP_ERROR = document.querySelector("#state-error-spa");
+export const COUNTRY_SIGNUP_FIELD = document.querySelector("#country-field");
+export const COUNTRY_SIGNUP_ERROR = document.querySelector("#country-error-span");
+export const CITY_SIGNUP_FIELD = document.querySelector("#city-field");
+export const CITY_SIGNUP_ERROR = document.querySelector("#city-error-span");
+export const STREET_SIGNUP_FIELD = document.querySelector("#street-field");
+export const STREET_SIGNUP_ERROR = document.querySelector("#street-error-span");
+export const HOUSE_NUMBER_SIGNUP_FIELD = document.querySelector("#house-number-field");
+export const HOUSE_NUMBER_SIGNUP_ERROR = document.querySelector("#house-number-error-span");
+export const ZIP_CODE_SIGNUP_FIELD = document.querySelector("#zip-code-field");
+export const ZIP_CODE_SIGNUP_ERROR = document.querySelector("#zip-code-error-spa");
+export const PHONE_SIGNUP_FIELD = document.querySelector("#phone-field");
+export const PHONE_SIGNUP_ERROR = document.querySelector("#Phone-error-span");
+export const BUSINESS_SIGNUP_FIELD = document.querySelector("#business-field");
+export const BUSINESS_SIGNUP_ERROR = document.querySelector("#Business-error-span");
+export const SUBMIT_SIGNUP_BTN = document.querySelector("#submit-signup-btn");
+export const CANCEL_SIGNUP_BTN = document.querySelector("#cancel-signup-btn");
+
+//#endregion
