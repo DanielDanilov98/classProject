f4383bc1bf6d7659ed6c4a85d3890a8653824682
Daniel Danilov <danieldanilov1998@gmail.com>
2023-01-13 15:11:35 +0200

    commit

--- classProject/src/routes/router.js
+++ classProject/src/routes/router.js
@@ -1,12 +1,13 @@
-import { ABOUT_PAGE, ERROR_404_PAGE, HOME_PAGE, CREATE_PIC_PAGE, LOGIN_PAGE, SIGN_UP_PAGE } from "../services/domService.js";
+import { ABOUT_PAGE, ERROR_404_PAGE, HOME_PAGE, CREATE_PIC_PAGE, LOGIN_PAGE, SIGNUP_PAGE, ADD_PIC_LINK_CONTAINER, LOGIN_LINK_CONTAINER, LOGOUT_LINK_CONTAINER, NO_DATA_CONTAINER, DATA_CONTAINER, TABLE_DISPLAY_MODE, SLIDER_DISPLAY_MODE } from "../services/domService.js";
 import PAGES from "../models/pageModel.js";
+import { getItemFromLocalStorage } from "../services/localStorageService.js";
+import DISPLAY from "../models/displayModel.js";
 
 const pageToDOMMap = [
   {
     page: PAGES.HOME,
     dom: HOME_PAGE,
   },
-
   {
     page: PAGES.ABOUT,
     dom: ABOUT_PAGE,
@@ -20,14 +21,15 @@ const pageToDOMMap = [
     dom: LOGIN_PAGE,
   },
   {
-    page: PAGES.SIGNUP,
-    dom: SIGN_UP_PAGE,
-  },
-  {
     page: PAGES.ERROR_404,
     dom: ERROR_404_PAGE,
   },
+  {
+    page: PAGES.SIGN_UP,
+    dom: SIGNUP_PAGE,
+  },
 ];
+
 export const onChangePage = (page) => {
   pageToDOMMap.forEach((pageMap) => (pageMap.dom.className = "d-none")); // hide all pages
 
@@ -37,4 +39,35 @@ export const onChangePage = (page) => {
 
   // show 404 page if we got this far (the page was not found, so the 'return' statement was not executed)
   ERROR_404_PAGE.className = "d-block";
+};
+
+export const setNavDisplay = () => {
+  ADD_PIC_LINK_CONTAINER.className = "d-none";
+  const token = getItemFromLocalStorage("user");
+  if (!token) {
+    LOGIN_LINK_CONTAINER.className = "navbar-nav";
+    LOGOUT_LINK_CONTAINER.className = "d-none";
+    return;
+  }
+  LOGIN_LINK_CONTAINER.className = "d-none";
+  LOGOUT_LINK_CONTAINER.className = "navbar-nav";
+  const user = JSON.parse(token);
+  if (user.isBusiness) return (ADD_PIC_LINK_CONTAINER.className = "nav-item");
+};
+
+export const onChangeDisplayMode = (pictures, display = []) => {
+  NO_DATA_CONTAINER.className = "d-none";
+  DATA_CONTAINER.className = "d-none";
+  TABLE_DISPLAY_MODE.className = "d-none";
+  SLIDER_DISPLAY_MODE.className = "d-none";
+
+  if (!pictures.length) return (NO_DATA_CONTAINER.className = "d-block");
+  DATA_CONTAINER.className = "d-block";
+
+  switch (display) {
+    case DISPLAY.SLIDER:
+      return (SLIDER_DISPLAY_MODE.className = "d-block");
+    case DISPLAY.TABLE:
+      return (TABLE_DISPLAY_MODE.className = "d-block");
+  }
 };
--- classProject/src/services/domService.js
+++ classProject/src/services/domService.js
@@ -1,9 +1,16 @@
 //#region nav links
 export const HOME_PAGE_LINK = document.querySelector("#home-link");
 export const ABOUT_PAGE_LINK = document.querySelector("#about-page-link");
+
 export const CREATE_PIC_PAGE_LINK = document.querySelector("#create-pic-page-link");
 export const LOGIN_PAGE_LINK = document.querySelector("#login-page-link");
 export const SIGNUP_PAGE_LINK = document.querySelector("#signup-page-link");
+
+export const ADD_PIC_LINK_CONTAINER = document.querySelector("#add-pic-link-container");
+export const LOGIN_LINK_CONTAINER = document.querySelector("#login-link-container");
+export const LOGOUT_LINK_CONTAINER = document.querySelector("#logout-link-container");
+export const EDIT_USER_PAGE_LINK = document.querySelector("#edit-user-link");
+export const LOGOUT_LINK = document.querySelector("#logout-link");
 //#endregion
 
 //#region pages
@@ -11,7 +18,7 @@ export const HOME_PAGE = document.querySelector("#home-page");
 export const ABOUT_PAGE = document.querySelector("#about-page");
 export const CREATE_PIC_PAGE = document.querySelector("#create-pic-page");
 export const LOGIN_PAGE = document.querySelector("#login-page");
-export const SIGN_UP_PAGE = document.querySelector("#signup-page");
+export const SIGNUP_PAGE = document.querySelector("#signup-page");
 export const ERROR_404_PAGE = document.querySelector("#error-404-page");
 
 //#endregion
@@ -45,33 +52,61 @@ export const SUBMIT_CREATE_PIC_BTN = document.querySelector("#submit-create-pic-
 export const CANCEL_BTN = document.querySelector("#cancel-create-pic-btn");
 //#endregion
 
-//#region create user
+//#region Signup form
 
-export const FIRST_NAME_SIGNUP_FIELD = document.querySelector("#first-name-field");
-export const FIRST_NAME_SIGNUP_ERROR = document.querySelector("#first-name-error-span");
-export const LAST_NAME_SIGNUP_FIELD = document.querySelector("#last-name-field");
-export const LAST_NAME_SIGNUP_ERROR = document.querySelector("#last-name-error-span");
-export const EMAIL_SIGNUP_FIELD = document.querySelector("#email-field");
-export const EMAIL_SIGNUP_ERROR = document.querySelector("#email-error-span");
-export const PASSWORD_SIGNUP_FIELD = document.querySelector("#password-field");
-export const PASSWORD_SIGNUP_ERROR = document.querySelector("#password-error-span");
-export const STATE_SIGNUP_FIELD = document.querySelector("#state-field");
-export const STATE_SIGNUP_ERROR = document.querySelector("#state-error-spa");
-export const COUNTRY_SIGNUP_FIELD = document.querySelector("#country-field");
-export const COUNTRY_SIGNUP_ERROR = document.querySelector("#country-error-span");
-export const CITY_SIGNUP_FIELD = document.querySelector("#city-field");
-export const CITY_SIGNUP_ERROR = document.querySelector("#city-error-span");
-export const STREET_SIGNUP_FIELD = document.querySelector("#street-field");
-export const STREET_SIGNUP_ERROR = document.querySelector("#street-error-span");
-export const HOUSE_NUMBER_SIGNUP_FIELD = document.querySelector("#house-number-field");
-export const HOUSE_NUMBER_SIGNUP_ERROR = document.querySelector("#house-number-error-span");
-export const ZIP_CODE_SIGNUP_FIELD = document.querySelector("#zip-code-field");
-export const ZIP_CODE_SIGNUP_ERROR = document.querySelector("#zip-code-error-spa");
-export const PHONE_SIGNUP_FIELD = document.querySelector("#phone-field");
-export const PHONE_SIGNUP_ERROR = document.querySelector("#Phone-error-span");
-export const BUSINESS_SIGNUP_FIELD = document.querySelector("#business-field");
-export const BUSINESS_SIGNUP_ERROR = document.querySelector("#Business-error-span");
-export const SUBMIT_SIGNUP_BTN = document.querySelector("#submit-signup-btn");
-export const CANCEL_SIGNUP_BTN = document.querySelector("#cancel-signup-btn");
+export const FIRST_SIGNUP_FIELD = document.querySelector("#first-name-signup-field");
+export const FIRST_SIGNUP_ERROR = document.querySelector("#first-name-signup-error-span");
+export const LAST_SIGNUP_FIELD = document.querySelector("#last-name-signup-field");
+export const LAST_SIGNUP_ERROR = document.querySelector("#last-name-signup-error-span");
+export const STATE_SIGNUP_FIELD = document.querySelector("#state-signup-field");
+export const STATE_SIGNUP_ERROR = document.querySelector("#state-signup-error-span");
+export const COUNTRY_SIGNUP_FIELD = document.querySelector("#country-signup-field");
+export const COUNTRY_SIGNUP_ERROR = document.querySelector("#country-signup-error-span");
+export const CITY_SIGNUP_FIELD = document.querySelector("#city-signup-field");
+export const CITY_SIGNUP_ERROR = document.querySelector("#city-signup-error-span");
+export const STREET_SIGNUP_FIELD = document.querySelector("#street-signup-field");
+export const STREET_SIGNUP_ERROR = document.querySelector("#street-signup-error-span");
+export const HOUSE_SIGNUP_FIELD = document.querySelector("#house-signup-field");
+export const HOUSE_SIGNUP_ERROR = document.querySelector("#house-signup-error-span");
+export const ZIP_SIGNUP_FIELD = document.querySelector("#zip-signup-field");
+export const ZIP_SIGNUP_ERROR = document.querySelector("#zip-signup-error-span");
+export const EMAIL_SIGNUP_FIELD = document.querySelector("#email-signup-field");
+export const EMAIL_SIGNUP_ERROR = document.querySelector("#email-signup-error-span");
+export const PHONE_SIGNUP_FIELD = document.querySelector("#phone-signup-field");
+export const PHONE_SIGNUP_ERROR = document.querySelector("#phone-signup-error-span");
+export const PASSWORD_SIGNUP_FIELD = document.querySelector("#password-signup-field");
+export const PASSWORD_SIGNUP_ERROR = document.querySelector("#password-signup-error-span");
+export const PASSWORD_RE_ENTER_SIGNUP_FIELD = document.querySelector("#password-reenter-signup-field");
+export const PASSWORD_RE_ENTER_SIGNUP_ERROR = document.querySelector("#password-reenter-signup-error-span");
+export const BIZ_SIGNUP_FIELD = document.querySelector("#isBiz-signup-field");
+export const SUBMIT_BTN_SIGNUP = document.querySelector("#submit-signup-user-btn");
+export const CANCEL_BTN_SIGNUP = document.querySelector("#cancel-signup-user-btn");
+//#endregion
 
+//#region login form
+export const EMAIL_LOGIN_FIELD = document.querySelector("#login-email");
+export const EMAIL_LOGIN_ERROR = document.querySelector("#login-email-error");
+export const PASSWORD_LOGIN_FIELD = document.querySelector("#login-password");
+export const PASSWORD_LOGIN_ERROR = document.querySelector("#login-password-error");
+export const SUBMIT_LOGIN_BTN = document.querySelector("#submit-login-btn");
+export const CANCEL_LOGIN_BTN = document.querySelector("#cancel-login-btn");
+
+export const LINK_TO_SIGNUP_PAGE = document.querySelector("#link-to-signup-page");
+//#endregion
+
+//#region controllers
+export const TABLE_ICON = document.querySelector("#table-icon");
+export const SLIDER_ICON = document.querySelector("#slider-icon");
+export const CARDS_ICON = document.querySelector("#cards-icon");
+export const SORT_DOWN_ICON = document.querySelector("#sort-down-icon");
+export const SORT_UP_ICON = document.querySelector("#sort-up-icon");
+//#endregion
+
+//#region display mode
+export const NO_DATA_CONTAINER = document.querySelector("#no-data-container");
+export const DATA_CONTAINER = document.querySelector("#data-container");
+export const TABLE_DISPLAY_MODE = document.querySelector("#table-display");
+export const SLIDER_DISPLAY_MODE = document.querySelector("#slider-display");
+
+export const LINK_TO_CREATE_PIC_PAGE = document.querySelector("#goto-create-pic-page-link");
 //#endregion
